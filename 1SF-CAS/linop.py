aff41298fc0ea8db70907c9590793c6c08064577
Shannon Houck <shannonhouck@shannon-book.local>
2018-10-02 17:52:10 -0400

    Fixed misarrangement of terms in 1SF-CAS (didn't affect values but was in wrong form for Block I terms)

--- 1SF-CAS/linop.py
+++ 1SF-CAS/linop.py
@@ -61,13 +61,13 @@ class LinOpH (LinearOperator):
             #   sig(ia:ba) += -v(ia:ba) (eps(a:b)-eps(i:a))
             Fi_tmp = Fa[nb_occ:na_occ, nb_occ:na_occ]
             Fa_tmp = Fb[nb_occ:na_occ, nb_occ:na_occ]
-            F_tmp = np.einsum("ia,aa->ia", v_b1, Fa_tmp) - np.einsum("ia,ii->ia", v_b1, Fi_tmp)
+            F_tmp = np.einsum("ia,ab->ia", v_b1, Fa_tmp) - np.einsum("ia,ij->ia", v_b1, Fi_tmp)
             F_tmp.shape = (v.shape[0], 1)
             # two-electron part (OK!!)
-            #   sig(ai:ba) += -v(bj:ba) I(jaib:abab)
-            tei_tmp = -1.0*self.tei.get_subblock((nb_occ, na_occ), (nb_occ, na_occ), (nb_occ, na_occ), (nb_occ, na_occ))
+            #   sig(ai:ba) += -v(bj:ba) I(ajbi:baba)
             # using reshape because tei is non-contiguous in memory (look into this while doing speedup)
-            tei_tmp = np.reshape(np.einsum("jb,jaib->ia", v_b1, tei_tmp), (v.shape[0], 1))
+            tei_tmp = self.tei.get_subblock((nb_occ, na_occ), (nb_occ, na_occ), (nb_occ, na_occ), (nb_occ, na_occ))
+            tei_tmp = np.reshape(-1.0*np.einsum("jb,ajbi->ia", v_b1, tei_tmp), (v.shape[0], 1))
             return F_tmp + tei_tmp
         # do excitation scheme: 1SF-CAS + h
         if(conf_space=="p"):
@@ -106,22 +106,20 @@ class LinOpH (LinearOperator):
             v_ref2 = np.reshape(v_b2, (socc, socc, na_virt, socc))
 
             ################################################ 
+            # PROBABLY OKAY!!
             # Do the following term:
             #       H(1,1) v(1) = sig(1)
             ################################################ 
            
-            # one-electron part (probably ok but wip)
-            #   sig(ia':ba) += -v(ia':ba) (eps(a':b)-eps(i:a))
+            #   sig(ia':ba) += v(ia':ab) F(a'b':bb) - v(ia':ab) F(ij:aa)
             Fi_tmp = Fa[nb_occ:na_occ, nb_occ:na_occ]
             Fa_tmp = Fb[nb_occ:nbf, nb_occ:nbf]
-            F_tmp = np.einsum("ia,aa->ia", v_ref1, Fa_tmp) - np.einsum("ia,ii->ia", v_ref1, Fi_tmp)
-            F_tmp.shape = (v_b1.shape[0], 1)
-            # two-electron part (probably ok but wip)
+            sig_1 = np.einsum("ia,ab->ia", v_ref1, Fa_tmp) - np.einsum("ia,ij->ia", v_ref1, Fi_tmp)
             #   sig(a'i:ba) += -v(b'j:ba) I(ja'ib':abab)
-            tei_tmp = self.tei.get_subblock((nb_occ,na_occ), (nb_occ,nbf), (nb_occ,na_occ), (nb_occ,nbf))
-            out1 = np.einsum("jb,jaib->ia", v_ref1, tei_tmp) 
-            out1 = np.reshape(out1, (v_b1.shape[0], 1))
+            tei_tmp = self.tei.get_subblock((nb_occ, nbf), (nb_occ, na_occ), (nb_occ, nbf), (nb_occ, na_occ))
+            sig_1 = sig_1 - np.einsum("jb,ajbi->ia", v_ref1, tei_tmp)
             
+            '''
             ################################################ 
             # Do the following term:
             #       H(1,2) v(2) = sig(1)
@@ -130,58 +128,76 @@ class LinOpH (LinearOperator):
             # two-electron part (like 90% probably ok)
             #   sig(a'i:ba) += -2*t(ijBc:aaab) I(a'jcB:baaa)
             tei_tmp = self.tei.get_subblock((nb_occ,nbf), (nb_occ,na_occ), (nb_occ,na_occ), (na_occ,nbf))
-            out2 = -2.0*np.einsum("ijBc,ajcB->ia", v_ref2, tei_tmp)
-            out2 = np.reshape(out2, (v_b1.shape[0], 1))
-            sig1 = F_tmp + out1 + out2
-            
+            sig_1 = sig_1 - 2.0*np.einsum("ijBc,ajcB->ia", v_ref2, tei_tmp)
+            #   sig(ai:ba) += t(kjAa:aaab) I(kjiA:aaaa) (partial block!)
+            tei_tmp = self.tei.get_subblock((nb_occ,na_occ), (nb_occ,na_occ), (nb_occ,na_occ), (na_occ,nbf))
+            partial_tmp = np.einsum("kjAa,kjiA->ia", v_ref2, tei_tmp)
+            tei_tmp = self.tei.get_subblock((nb_occ,na_occ), (nb_occ,na_occ), (na_occ,nbf), (nb_occ,na_occ))
+            partial_tmp = partial_tmp - np.einsum("kjAa,kjAi->ia", v_ref2, tei_tmp)
+            sig_1 = sig_1 + np.hstack((partial_tmp, np.zeros((partial_tmp.shape[0], na_virt))))
+
+            ################################################ 
+            # sig(1) is complete-- free to reshape!
+            ################################################ 
+            #sig_1 = np.reshape(sig_1, (v_b1.shape[0], 1))
+
             ################################################ 
             # Do the following term:
             #       H(2,1) v(1) = sig(2)
             ################################################ 
             # two-electron part (wip)
-            #   sig(ijAa:aaab) += t(ib':ab)*I(Aab'j:abba)
-            tei_tmp = self.tei.get_subblock((na_occ, nbf), (nb_occ, na_occ), (nb_occ, nbf), (nb_occ, na_occ))
-            out3 = -1.0*np.einsum("ib,Aabj->ijAa", v_ref1, tei_tmp)
+            #   sig(ijAa:aaab) += t(ia':ab)*I(Abja':abaa)
+            #tei_tmp = self.tei.get_subblock((na_occ, nbf), (nb_occ, na_occ), (nb_occ, na_occ), (nb_occ, nbf))
+            #out3 = -1.0*np.einsum("ib,Aajb->ijAa", v_ref1, tei_tmp)
             #   sig(ijAa:aaab) += t(ia:ab)*I(kAij:abaa)
             tei_tmp = self.tei.get_subblock((nb_occ, na_occ), (na_occ, nbf), (nb_occ, na_occ), (nb_occ, na_occ))
-            out3 = out3 - 0.5*np.einsum("ka,kAij->ijAa", v_ref1[:, :socc], tei_tmp)
-            out3 = np.reshape(out3, (v_b2.shape[0], 1))
+            sig_2 = -0.5*(np.einsum("ka,kAij->ijAa", v_ref1[:, :socc], tei_tmp) - np.einsum("ka,kAji->ijAa", v_ref1[:, :socc], tei_tmp))
+            #   sig(ijAa:aaab) += t(ib':ab)*I(Aab'j:abba)
+            tei_tmp = self.tei.get_subblock((nb_occ, na_occ), (na_occ, nbf), (nb_occ, nbf), (nb_occ, na_occ))
+            sig_2 = sig_2 - np.einsum("ib,aAbj->ijAa", v_ref1, tei_tmp)
 
             ################################################ 
             # Do the following term:
             #       H(2,2) v(2) = sig(2)
             ############################################### 
 
-            # one-electron part
+            # one-electron part (OK??)
             #    sig(ijAa:aaab) += t(ikAb:aaab)*Fi(kj:aa)
             #    sig(ijAa:aaab) += t(ijAb:aaab)*Fa(ab:bb)
             Fi2_tmp = Fa[nb_occ:na_occ, nb_occ:na_occ]
             Fa2_tmp = Fb[nb_occ:na_occ, nb_occ:na_occ]
-            F_tmp2 = np.einsum("ijAb,cb->ijAc", v_ref2, Fa2_tmp) - np.einsum("ikAb,kj->ijAb", v_ref2, Fi2_tmp)
-            F_tmp2.shape = (v_b2.shape[0], 1)
-
-            # two-electron part (probably ok)
+            sig_2 = sig_2 + np.einsum("ijAb,ab->ijAb", v_ref2, Fa2_tmp) - np.einsum("ijAb,jk->ijAb", v_ref2, Fi2_tmp)
+            # two-electron part (OK)
             #    sig(ijAa:aaab) += t(ijCd:aaab)*I(AaCd:abab)
             tei_tmp = self.tei.get_subblock((na_occ,nbf), (nb_occ,na_occ), (na_occ,nbf), (nb_occ,na_occ))
-            out4 = np.einsum("ijCd,AaCd->ijAa", v_ref2, tei_tmp) #- np.einsum("ijCd,AadC->ijAa", v_ref2, tei_tmp)
-            out4.shape = (v_b2.shape[0], 1)
+            sig_2 = sig_2 + np.einsum("ijCd,AaCd->ijAa", v_ref2, tei_tmp)
 
             #    sig(ijAa:aaab) += 0.5*( t(klAb:aaab)*I(klij:aaaa) )
             tei_tmp = self.tei.get_subblock((nb_occ,na_occ), (nb_occ,na_occ), (nb_occ,na_occ), (nb_occ,na_occ))
-            out5 = 0.5*(np.einsum("klAa,klij->ijAa", v_ref2, tei_tmp) - np.einsum("klAa,klji->ijAa", v_ref2, tei_tmp))
-            out5.shape = (v_b2.shape[0], 1)
+            sig_2 = sig_2 + 0.5*(np.einsum("klAa,klij->ijAa", v_ref2, tei_tmp)-np.einsum("klAa,klji->ijAa", v_ref2, tei_tmp))
 
             #    sig(ijAa:aaab) += t(ikBa:aaab)*I(AkBj:aaaa) - t(jkBa:aaab)*I(AkBi:aaaa)
+            # coulomb term
             tei_tmp = self.tei.get_subblock((na_occ,nbf), (nb_occ,na_occ), (na_occ,nbf), (nb_occ,na_occ))
-            out6 = np.einsum("ikBa,AkBj->ijAa", v_ref2, tei_tmp) #- np.einsum("ikBa,AkjB->ijAa", v_ref2, tei_tmp)
-            out6 = out6 - (np.einsum("jkBa,AkBi->ijAa", v_ref2, tei_tmp)) #- np.einsum("jkBa,AkiB->ijAa", v_ref2, tei_tmp))
-            out6.shape = (v_b2.shape[0], 1)
+            sig_2 = sig_2 + np.einsum("ikBa,AkBj->ijAa", v_ref2, tei_tmp)
+            sig_2 = sig_2 - (np.einsum("jkBa,AkBi->ijAa", v_ref2, tei_tmp))
+            # exchange term
+            tei_tmp = self.tei.get_subblock((na_occ,nbf), (nb_occ,na_occ), (nb_occ,na_occ), (na_occ,nbf))
+            sig_2 = sig_2 - np.einsum("ikBa,AkjB->ijAa", v_ref2, tei_tmp)
+            sig_2 = sig_2 + (np.einsum("jkBa,AkiB->ijAa", v_ref2, tei_tmp))
+            '''
 
-            sig2 = F_tmp2 + out3 + out4 + out5 + out6
+            ################################################ 
+            # sig(2) complete, reshape!
+            ################################################ 
+            #sig_2 = np.reshape(sig_2, (v_b2.shape[0], 1))
 
-            out = np.vstack((sig1, sig2))
+            # time to PROBLEM SOLVEEE
+            sig_1 = np.reshape(sig_1, (v_b1.shape[0], 1))
+            sig_2 = np.zeros((v_b2.shape[0], 1))
 
-        return out
+            # combine and return
+            return np.vstack((sig_1, sig_2))
 
     # These two are vestigial-- I'm sure they served some purpose in the parent class,
     # but we only really need matvec for our purposes!
