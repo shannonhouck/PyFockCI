d068affb6d8cb8b6bba2af1356af7ce68541c6f1
Shannon Houck <shouck@vt.edu>
2019-08-21 13:14:58 -0400

    Updated Travis build

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='sf-ip-ea',
+    version='0.1.0',
+    author='Shannon E. Houck',
+    author_email='shouck@vt.edu',
+    packages=['sf-ip-ea', 'sf-ip-ea.test'],
+    scripts=[],
+    url='',
+    license='LICENSE.txt',
+    description='Stand-alone RAS-SF-IP/EA code.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+    ],
+)
