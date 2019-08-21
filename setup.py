6700a13d3cbbf82ed40fbe95bc38ff4a57706308
Shannon Houck <shouck@vt.edu>
2019-08-21 16:28:04 -0400

    Updated more tests

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='sf-ip-ea',
+    name='sf_ip_ea',
     version='0.1.0',
     author='Shannon E. Houck',
     author_email='shouck@vt.edu',
-    packages=['sf-ip-ea', 'sf-ip-ea.test'],
+    packages=['sf_ip_ea', 'sf_ip_ea.test'],
     scripts=[],
     url='',
     license='LICENSE.txt',
     description='Stand-alone RAS-SF-IP/EA code.',
     long_description=open('README.md').read(),
-    install_requires=[
-    ],
 )
---
+++ sf_ip_ea/test/test_1sf_ip/test_cas_sfip.py
@@ -0,0 +1,57 @@
+import psi4
+import sf_ip_ea
+from sf_ip_ea import fock_ci
+
+# threshold for value equality
+threshold = 1e-7
+# setting up molecule
+n2_7 = psi4.core.Molecule.create_molecule_from_string("""
+0 7
+N 0 0 0
+N 0 0 2.5
+symmetry c1
+""")
+n2_3 = psi4.core.Molecule.create_molecule_from_string("""
+0 5
+N 0 0 0
+N 0 0 1.5
+symmetry c1
+""")
+o2 = psi4.core.Molecule.create_molecule_from_string("""
+0 5
+O
+O 1 1.2
+symmetry c1
+""")
+
+# Test: 1SF-CAS
+def test_1():
+    psi4.core.clean()
+    psi4.core.clean_options()
+    psi4.core.clean_variables()
+    options = {"basis": "cc-pvdz", 'num_roots': 4, 'diis_start': 20, 'e_convergence': 1e-10, 'd_convergence': 1e-10}
+    expected = [-108.257015341634414, -108.250447679154192, -108.231958280565408, -108.231958280565280]
+    wfn = fock_ci( 2, 1, n2_7, conf_space="h", ref_opts=options, sf_opts={'NUM_ROOTS': 4} )
+    for i, true in enumerate(wfn.e):
+        assert abs(true - expected[i]) < threshold
+
+def test_2():
+    psi4.core.clean()
+    psi4.core.clean_options()
+    psi4.core.clean_variables()
+    options = {"basis": "cc-pvdz", 'num_roots': 4, 'diis_start': 20, 'e_convergence': 1e-10, 'd_convergence': 1e-10}
+    expected = [-108.344924167067447, -108.310124617612658, -108.310124617612630, -108.299904314118322]
+    wfn = fock_ci( 2, 1, n2_3, conf_space="h", ref_opts=options, sf_opts={'NUM_ROOTS': 4} )
+    for i, true in enumerate(wfn.e):
+        assert abs(true - expected[i]) < threshold
+
+def test_3():
+    psi4.core.clean()
+    psi4.core.clean_options()
+    psi4.core.clean_variables()
+    options = {"basis": "cc-pvdz", 'e_convergence': 1e-10, 'd_convergence': 1e-10}
+    expected = [-149.207583364661616, -149.171950340935524]
+    wfn = fock_ci( 2, 1, o2, conf_space="h", ref_opts=options, sf_opts={'NUM_ROOTS': 2} )
+    for i, true in enumerate(wfn.e):
+        assert abs(true - expected[i]) < threshold
+
