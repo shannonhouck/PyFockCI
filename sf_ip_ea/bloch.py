795c68008c198a76f23eb88d2a5c5cdb15b757cf
shannonhouck <shouck@vt.edu>
2019-10-15 13:06:41 -0400

    Updated indexing issue

--- sf_ip_ea/bloch.py
+++ sf_ip_ea/bloch.py
@@ -132,7 +132,7 @@ def do_bloch(wfn, n_sites, site_list=None, site_list_orbs=None,
         perm = []
         for site in site_list_orbs:
             for orb in site:
-                perm.append(ras1 - orb)
+                perm.append(orb - ras1)
         print("Reordering RAS2 determinants as follows:")
         print(perm)
         v_n = v_n[np.argsort(perm), :] # permute!
