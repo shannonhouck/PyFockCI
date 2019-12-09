6ad2c4cad48994a2d6d01abe5a58c84424d55efe
shannonhouck <shouck@vt.edu>
2019-12-09 12:41:41 -0500

    Switched to DFHelper (hopefully more efficient?)

--- input.py
+++ input.py
@@ -9,16 +9,19 @@ import numpy.linalg as LIN
 import pickle
 
 n2_7 = psi4.core.Molecule.from_string("""
-0 7
-N 0 0 0
-N 0 0 0.75
+ 0 3
+Au 0 0 0
+H 0 0 0.75
 symmetry c1
 """)
 
-options = {"BASIS": "6-31G*", 'e_convergence': 1e-10, 'd_convergence': 1e-10, 'reference': 'rohf', 'scf_type': 'df'}
-sf_options = {'SF_DIAG_METHOD': 'DAVIDSON', 'NUM_ROOTS': 7, 'INTEGRAL_TYPE': 'DF'}
+options = {"BASIS": "def2-tzvp", 
+           'e_convergence': 1e-10, 'd_convergence': 1e-10, 
+           'reference': 'rohf', 'scf_type': 'df'}
+sf_options = {'SF_DIAG_METHOD': 'DAVIDSON', 
+              'NUM_ROOTS': 7, 'INTEGRAL_TYPE': 'DF'}
 
-wfn = fock_ci( 1, 1, n2_7, conf_space="h", ref_opts=options, sf_opts=sf_options)
+wfn = fock_ci( 1, 1, n2_7, conf_space="h,p", ref_opts=options, sf_opts=sf_options)
 
 '''
 options = {"basis": "cc-pvtz", "BASIS_GUESS": "sto-3g", 'e_convergence': 1e-10, 'd_convergence': 1e-10, 'scf_type': 'direct', 'guess': 'gwh', 'reference': 'rohf'}
